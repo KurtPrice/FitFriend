9e73c1983271b21e919b0df275f11c2c830240e6
dan ban <Dayandersen@gmail.com>
2018-02-10 18:40:47 -0800

    Hackathon-api Nodejs mocked up

---
+++ hack_api/api/routes/routes.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = function(app) {
+  var api = require('../controllers/controller');
+
+  // todoList Routes
+  app.route('/users')
+    .get(api.list_all_users)
+    .post(api.create_a_user);
+
+
+  app.route('/users/:userId')
+    .get(api.read_a_user)
+    .put(api.update_a_user)
+    .delete(api.delete_a_user);
+};
