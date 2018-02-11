50fec84c3f97da678336f9ed43b0ff670a92714e
hailes2 <hailes2@students.wwu.edu>
2018-02-10 22:27:18 -0800

    Added edit profile and sign up skeleton

--- app/src/main/java/com/bignybble/fitfriend/SignupActivity.java
+++ app/src/main/java/com/bignybble/fitfriend/SignupActivity.java
@@ -1,7 +1,9 @@
 package com.bignybble.fitfriend;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+
+    public void launchEditProfileActivity(View view) {
+        Intent intent = new Intent(this, EditProfileActivity.class);
+        startActivity(intent);
     }
 }
